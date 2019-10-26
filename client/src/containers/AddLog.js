24c1275b5ecfc257f18771f2fa7701592640bb82
toomusa <3895499+toomusa@users.noreply.github.com>
2019-10-26 14:24:21 -0700

    create page containers

---
+++ client/src/containers/AddLog.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AddLog extends Component {
+  render() {  
+    return (
+      <div>
+        Add Log Page
+      </div>
+    )
+  }
+}
---
+++ client/src/containers/Tax.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Tax extends Component {
+  render() {
+    return (
+      <div>
+        Tax Page
+      </div>
+    )
+  }
+}
