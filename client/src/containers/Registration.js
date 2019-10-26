64ebcdc70e6c29d4bf3e6e9cb72fd18b19d260c8
Joshua Wong <joshua.wong.developer@gmail.com>
2019-10-26 15:29:02 -0700

    Added mock data for registration page.

--- client/src/containers/Registration.js
+++ client/src/containers/Registration.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react'
 
 export default class Registration extends Component {
+   constructor(props) {
+      super(props);
+      
+      this.vendorId = 1;
+      this.vendor = {
+         business_name: 'ABC',
+         address: {
+            address_line_1: '101 Post Street',
+            address_line_2: 'Unit #123',
+            city: 'San Francisco',
+            state: 'CA',
+            postal_code: '94101',
+         },
+      };
+   }
+
    componentDidMount() {
    }
    
    render() {
     return (
       <div>
-        Registration Page
+         <div>Registration Page</div>
+         <div>Vendor ID</div><div>{this.vendorId}</div>
+         <div>Business Name</div><div>{this.vendor.business_name}</div>
+         <div>Address Line 1</div><div>{this.vendor.address.address_line_1}</div>
+         <div>Address Line 2</div><div>{this.vendor.address.address_line_2}</div>
+         <div>City</div><div>{this.vendor.address.city}</div>
+         <div>State</div><div>{this.vendor.address.state}</div>
+         <div>Postal Code</div><div>{this.vendor.address.postal_code}</div>
       </div>
     )
   }
