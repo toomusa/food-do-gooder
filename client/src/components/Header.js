2d6bdee2c1cd9e84c29d6894a63c5610a93d0337
toomusa <3895499+toomusa@users.noreply.github.com>
2019-10-27 12:31:14 -0700

    biz registration page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,8 @@ function Header(props) {
   return (
     <div className="page-header" id="heading">
       {/* <img src={FDG} alt="logo" id="logoImg" /> */}
-      <h1>Food Do-Gooders</h1>
+      <h1>Food Do-Gooder</h1>
+      <h5 id="tagline">Do Good with Good Food</h5>
       <Navbar path={props.path} activate={props.activate} />
     </div>
   );
--- client/src/components/StepOne.js
+++ client/src/components/StepOne.js
@@ -5,9 +5,9 @@ export default function StepOne() {
   return (
     <div>
       <h5>Fill out the information below</h5><br></br>
-      <h6><strong>Business Name:</strong> <span className="fading">McDonald's</span></h6>
+      <h6><strong>Business Name:</strong> <span className="fading">Wolfgang Puck Pizza</span></h6>
       <h6><strong>Date:</strong>  <span className="fading">10/27/2019</span></h6>
-      <h6><strong>Donated Items:</strong>  <span className="fading">Buns, lettuce, tomatoes, ketchup</span></h6>
+      <h6><strong>Donated Items:</strong>  <span className="fading">Lettuce, tomatoes, onions, peppers</span></h6>
       <h6><strong>Estimated Worth:</strong>  <span className="fading">$80</span></h6>
       <h6><strong>Estimated Servings:</strong>  <span className="fading">12</span></h6>
       <h6><strong>Photo:</strong><br></br><br></br>  <span className="fading"><img src={donation} alt="food donation" /></span></h6>
