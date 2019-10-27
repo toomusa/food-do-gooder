178704509292d5a75d0f1dbed12b6d82914cde1d
Greg <gregsinbox@protonmail.com>
2019-10-27 09:44:53 -0700

    Completed card and parent data

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -5,25 +5,28 @@ import '../../node_modules/jquery/dist/jquery.min.js';
 import glide from '../assets/images/glide.svg';
 
 const Card = () => {
+    constructor(props){
+      super(props);
+    } 
     return(
   <div align="center">
     <div className="card" style="max-width: 1000px;">
         <div className="row no-gutters">
             <div className="col-md-3" style="background: #868e96;">
-                <img src="{glide}" className="card-img-top h-100" alt="Image Error">
+                <img src="{this.props.picture}" className="card-img-top h-100" alt="Image Error">
             </div>
             <div className="col-md-5">
                 <div className="card-body">
-                    <h5 className="card-title">Glide</h5>
-                    <p className="card-text">Glide is a homeless shelter that specializes in aromatherpay</p>
+                    <h5 className="card-title">{this.props.name}</h5>
+                    <p className="card-text">{this.props.description}</p>
                 </div>
             </div>
              <div className="col-md-4">
                 <div className="card-body">
-                    <h5 className="card-title">Glide Info</h5>
+                    <h5 className="card-title">{this.props.name}&nbsp;Info</h5>
                     <p className="card-text">
                       <ul>
-                        <li>Picks up Twice a week</li>
+                        <li>{this.props.schedule}</li>
                         <li><a href="/">Click here for website</a></li>
                       </ul>
                     </p>
