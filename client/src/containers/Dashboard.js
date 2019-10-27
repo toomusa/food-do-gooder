4a08bb825fd5387a2e90bdabf69b29048b5c12c0
Greg <gregsinbox@protonmail.com>
2019-10-27 12:33:30 -0700

    added table

--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -18,6 +18,44 @@ export default class Dashboard extends Component {
           <img src={Dashboard2} alt="Graphic 2" className="dashboard-image" />
           <img src={Dashboard1} alt="Graphic 1" className="dashboard-image" />
         </div>
+	<div>
+	<table class="table table-borderless">
+          <thead>
+            <tr>
+              <th scope="col">Date</th>
+              <th scope="col">Donated Items</th>
+              <th scope="col">Estimated Worth</th>
+              <th scope="col">Received By</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>10/19/20</td>
+              <td>Garlic Rolls, Spaghetti, Pizza</td>
+              <td>$75</td>
+              <td>Glide</td>
+            </tr>
+            <tr>
+              <td>10/25/20</td>
+              <td>Taco Meat, Tortillas, Rice, Beans</td>
+              <td>$65</td>
+              <td>Food Runners</td>
+            </tr>
+            <tr>
+              <td>10/05/20</td>
+              <td>Chicken Tandoori, Naan, Butter Chicken</td>
+              <td>$85</td>
+              <td>St. Anthonsy</td>
+            </tr>
+            <tr>
+              <td>10/14/20</td>
+              <td>Bulgogi, Dumplings, Rice, Seaweed Snacks</td>
+              <td>$75></td>
+              <td>City Team</td>
+            </tr>
+          </tbody>
+        </table>
+	</div>
       </div>
     )
   }
