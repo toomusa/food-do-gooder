5334bd7367e32cb7eef5302576ad069c184d7cad
Joshua Wong <joshua.wong.developer@gmail.com>
2019-10-26 17:09:57 -0700

    Updated styling.

--- client/src/containers/Registration.js
+++ client/src/containers/Registration.js
@@ -6,13 +6,13 @@ export default class Registration extends Component {
       
       this.vendorId = 1;
       this.vendor = {
-         business_name: 'ABC',
+         business_name: 'Wolfgang Puck Pizza',
          address: {
-            address_line_1: '101 Post Street',
-            address_line_2: 'Unit #123',
+            address_line_1: '170 O\'Farrell St',
+            address_line_2: 'Lower Level',
             city: 'San Francisco',
             state: 'CA',
-            postal_code: '94101',
+            postal_code: '94102',
          },
       };
    }
@@ -23,12 +23,16 @@ export default class Registration extends Component {
    render() {
     return (
       <div className="container-fluid text-muted p-2">
-         <div className="d-flex flex-column justify-content-left text-muted p-3 border-bottom">Your Registration Info</div>
-         <div className="d-flex flex-column justify-content-center p-3">
-            <div className="d-flex flex-row border">
-               <div className="col-xs-1 col-sm-1 label bg-secondary text-white p-2">Vendor ID</div>
-               <div className="col-xs-4 col-sm-4 p-2">{this.vendorId}</div>
+         <div className="border-bottom p-2">
+            <div className="d-inline align-bottom p-2">
+               <img height="30px" width="30px" src="https://s3-media3.fl.yelpcdn.com/bphoto/AiY7NjrgmWP9_nE5jtM1iw/o.jpg"/>
             </div>
+            <div className="d-inline align-bottom p-2">
+               Registration - {this.vendor.business_name}
+            </div>
+         </div>
+
+         <div className="d-flex flex-column justify-content-center p-3">
             <div className="d-flex flex-row border">
                <div className="col-xs-1 col-sm-1 label bg-secondary text-white p-2">Business Name</div>
                <div className="col-xs-4 col-sm-4 p-2">{this.vendor.business_name}</div>
