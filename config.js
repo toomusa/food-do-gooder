8210ccf5c9944146d835f4a63672349695ef76fa
toomusa <3895499+toomusa@users.noreply.github.com>
2019-10-28 20:01:35 -0700

    heroku

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "ksiueruykftnzdkjghuhsregiyb4lhfdhulkujeche"
+}
--- routes/apiRoutes/authRoutes.js
+++ routes/apiRoutes/authRoutes.js
@@ -1,4 +1,4 @@
-const router = require("express").Router();
+ const router = require("express").Router();
 const authController = require("../../controllers/authController")
 
 router.route("/signup")
